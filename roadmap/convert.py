e83c82f56441ed5a21b2ede8ab6f7f1bc9eeb848
ghobona <ghobona@ogc.org>
2025-05-02 18:38:59 +0100

    Update convert.py

--- roadmap/convert.py
+++ roadmap/convert.py
@@ -1,7 +1,7 @@
 import csv
 import json
 
-with open('/Users/gobehobona/Documents/Product Management/Productboard/20250502_roadmap.csv', mode='r', newline='', encoding='utf-8') as csvfile:
+with open('/Users/user1/Documents/Product Management/Productboard/20250502_roadmap.csv', mode='r', newline='', encoding='utf-8') as csvfile:
     data = list(csv.DictReader(csvfile))
 
 with open('./roadmap/20250502_roadmap.json', mode='w', encoding='utf-8') as jsonfile:
