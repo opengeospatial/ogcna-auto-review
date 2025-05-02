63509545f3d0a218a8e9a638a3e6b9929d23ce0a
ghobona <ghobona@ogc.org>
2025-05-02 17:19:47 +0100

    added the roadmap

---
+++ roadmap/convert.py
@@ -0,0 +1,8 @@
+import csv
+import json
+
+with open('/Users/gobehobona/Documents/Product Management/Productboard/20250502_roadmap.csv', mode='r', newline='', encoding='utf-8') as csvfile:
+    data = list(csv.DictReader(csvfile))
+
+with open('./roadmap/20250502_roadmap.json', mode='w', encoding='utf-8') as jsonfile:
+    json.dump(data, jsonfile, indent=4)
