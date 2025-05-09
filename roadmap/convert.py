321664f725115b97606fe9ad21d7907962300ecd
ghobona <ghobona@ogc.org>
2025-05-09 10:47:52 +0100

    roadmap moved to different repo
    https://opengeospatial.github.io/roadmap/timeline/

--- roadmap/convert.py
+++
@@ -1,8 +0,0 @@
-import csv
-import json
-
-with open('/Users/user1/Documents/Product Management/Productboard/20250502_roadmap.csv', mode='r', newline='', encoding='utf-8') as csvfile:
-    data = list(csv.DictReader(csvfile))
-
-with open('./roadmap/20250502_roadmap.json', mode='w', encoding='utf-8') as jsonfile:
-    json.dump(data, jsonfile, indent=4)
